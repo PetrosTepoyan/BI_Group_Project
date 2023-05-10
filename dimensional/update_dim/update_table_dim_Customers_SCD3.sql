83537843365c052f1b17bb5521c600e545da93e0
PetrosTepoyan <petros@tepoyan.com>
2023-05-10 19:45:09 +0400

    fixed missing columns

--- dimensional/update_dim/update_table_dim_Customers_SCD3.sql
+++ dimensional/update_dim/update_table_dim_Customers_SCD3.sql
@@ -4,8 +4,8 @@ MERGE {db_dim}.{schema_dim}.dim_Customers_SCD3 AS DST
 USING {db_rel}.{schema_rel}.Customers AS SRC
 ON (SRC.CustomerID = DST.CustomerID_NK)
 WHEN NOT MATCHED THEN
-INSERT (CustomerID_NK, ContactName, ContactTitle)
-VALUES (SRC.CustomerID, SRC.ContactName, SRC.ContactTitle)
+INSERT (CustomerID_NK, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone, Fax)
+VALUES (SRC.CustomerID, SRC.ContactName, SRC.ContactTitle, SRC.Address, SRC.City, SRC.Region, SRC.PostalCode, SRC.Country, SRC.Phone, SRC.Fax)
 WHEN MATCHED  -- there can be only one matched case
 AND (DST.ContactTitle <> SRC.ContactTitle
  OR DST.ContactName <> SRC.ContactName)
--- dimensional/update_dim/update_table_dim_Suppliers_SCD1_with_delete.sql
+++ dimensional/update_dim/update_table_dim_Suppliers_SCD1_with_delete.sql
@@ -4,10 +4,10 @@ USING {db_rel}.{schema_rel}.Suppliers AS SOURCE
 ON (TARGET.SupplierID_NK = SOURCE.SupplierID) 
 --When records are matched, update the records if there is any change
 WHEN MATCHED AND TARGET.CompanyName <> SOURCE.CompanyName OR TARGET.ContactName <> SOURCE.ContactName OR TARGET.ContactTitle <> SOURCE.ContactTitle
-THEN UPDATE SET TARGET.CompanyName = SOURCE.CompanyName, TARGET.ContactName = SOURCE.ContactName, TARGET.ContactTitle = SOURCE.ContactTitle  
+THEN UPDATE SET TARGET.CompanyName = SOURCE.CompanyName, TARGET.ContactName = SOURCE.ContactName, TARGET.ContactTitle = SOURCE.ContactTitle, TARGET.Address = SOURCE.Address, TARGET.City = SOURCE.City, TARGET.Region = SOURCE.Region, TARGET.PostalCode = SOURCE.PostalCode, TARGET.Country = SOURCE.Country, TARGET.Phone = SOURCE.Phone, TARGET.Fax = SOURCE.Fax, TARGET.HomePage = SOURCE.HomePage
 --When no records are matched, insert the incoming records from source table to target table
 WHEN NOT MATCHED BY TARGET 
-THEN INSERT (SupplierID_NK, CompanyName, ContactName, ContactTitle) VALUES (SOURCE.SupplierID, SOURCE.CompanyName, SOURCE.ContactName, SOURCE.ContactTitle)
+THEN INSERT (SupplierID_NK, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone, Fax, HomePage) VALUES (SOURCE.SupplierID, SOURCE.CompanyName, SOURCE.ContactName, SOURCE.ContactTitle, SOURCE.Address, SOURCE.City, SOURCE.Region, SOURCE.PostalCode, SOURCE.Country, SOURCE.Phone, SOURCE.Fax, SOURCE.HomePage)
 --When there is a row that exists in target and same record does not exist in source then delete this record target
 WHEN NOT MATCHED BY SOURCE 
 THEN DELETE 
@@ -18,11 +18,27 @@ DELETED.SupplierID_NK AS TargetSupplierID,
 DELETED.CompanyName AS TargetCompanyName, 
 DELETED.ContactName AS TargetContactName, 
 DELETED.ContactTitle AS TargetContactTitle, 
+DELETED.Address AS TargetAddress, 
+DELETED.City AS TargetCity, 
+DELETED.Region AS TargetRegion, 
+DELETED.PostalCode AS TargetPostalCode, 
+DELETED.Country AS TargetCountry, 
+DELETED.Phone AS TargetPhone, 
+DELETED.Fax AS TargetFax, 
+DELETED.HomePage AS TargetHomePage, 
 
 INSERTED.SupplierID_NK AS SourceSupplierSK, 
 INSERTED.CompanyName AS SourceCompanyName,
 INSERTED.ContactName AS SourceContactName,
-INSERTED.ContactTitle AS SourceContactTitle;
+INSERTED.ContactTitle AS SourceContactTitle,
+INSERTED.Address AS SourceAddress, 
+INSERTED.City AS SourceCity, 
+INSERTED.Region AS SourceRegion, 
+INSERTED.PostalCode AS SourcePostalCode, 
+INSERTED.Country AS SourceCountry, 
+INSERTED.Phone AS SourcePhone, 
+INSERTED.Fax AS SourceFax, 
+INSERTED.HomePage AS SourceHomePage;
 
 SELECT @@ROWCOUNT;
 
